f106c4853a486f750c1cf3486a527a99e206ccf7
Alireza <alireza.khosravi.1386.5.10@gmail.com>
2025-12-13 13:42:03 +0330

    implement many features

--- src/windows/index.js
+++ src/windows/index.js
@@ -1,3 +1,9 @@
+import Contact from "#windows/Contact.jsx";
+import Finder from "#windows/Finder.jsx";
+import Image from "#windows/Image.jsx";
+import Resume from "#windows/Resume.jsx";
+import Safari from "#windows/Safari.jsx";
 import Terminal from "#windows/Terminal.jsx";
+import Text from "#windows/Text.jsx";
 
-export { Terminal };
+export { Contact, Finder, Image, Resume, Safari, Terminal, Text };
