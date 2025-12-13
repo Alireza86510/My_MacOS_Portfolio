f583fd9432fc8b6bd9e297a9d849e700b57091f7
Alireza <alireza.khosravi.1386.5.10@gmail.com>
2025-12-13 14:02:01 +0330

    one of the last features

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Dock from "#components/dock.jsx";
+import Home from "#components/home.jsx";
 import Navbar from "#components/navbar.jsx";
 import Welcome from "#components/welcome.jsx";
 import WindowControls from "./WindowControls";
 
-export { Dock, Navbar, Welcome, WindowControls };
+export { Dock, Home, Navbar, Welcome, WindowControls };
